725f329ddbda7f38a8b3d70beb6ccba6d4c8a778
Denis-92 <d.mozenic@gmail.com>
2023-02-02 14:21:57 +0100

    init project

---
+++ src/org/lessons/java/christmas/Traccia.java
@@ -0,0 +1,14 @@
+package org.lessons.java.christmas;
+
+public class Traccia {
+	
+//	Siamo a dicembre, è tempo di pensare alla lista dei desideri per Natale…
+//	Creare una classe Main con metodo main, in cui implementare il seguente programma:
+//	creare una nuova ArrayList dove salvare la lista dei desideri
+//	continuare a chiedere all’utente di inserire un nuovo desiderio nella lista, fino a che l’utente sceglie di fermarsi
+//	ad ogni iterazione mostrare la lunghezza della lista e chiedere all’utente se vuole continuare
+//	ad ogni iterazione aggiungere il desiderio alla lista
+//	al termine dell’inserimento ordinare la lista
+//	stampare a video la lista ordinata
+
+}

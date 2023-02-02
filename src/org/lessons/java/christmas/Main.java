513663a83802bdf4c1f46b8d430a199fc58d301d
Denis-92 <d.mozenic@gmail.com>
2023-02-02 15:11:28 +0100

    add size to array list

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class Main {
 
@@ -22,12 +23,15 @@ public class Main {
 			continua = !desiderio.equals("STOP");
 			if (continua) {
 				listaDesideri.add(desiderio);
-				System.out.println("Inserisci un altro desiderio o scrivi \"STOP\" per terminare la lista: ");
+				System.out.println("Hai " + listaDesideri.size() + " desideri nella tua lista.");
+				System.out.print("Inserisci un altro desiderio o scrivi \"STOP\" per terminare la lista: ");
 			}
 		} while (continua);
 		
 		inputContainer.close();
 		// Chiuso inputContainer
+		
+		System.out.println("Programma terminato!");
 
 	}
 
